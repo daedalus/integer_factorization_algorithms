ec89465c0760f1a3896efc49729e93564a150550
Dario Clavijo <clavijodario@gmail.com>
2021-04-15 13:35:55 -0300

    Dixon algorithm enhancement

--- dixon.py
+++ dixon.py
@@ -3,9 +3,6 @@ import sys
 
 def dixon(N,B=7):
 
-  tmp = []
-  pairs = []
-
   def primes(B):
     p = 2
     tmp = [p]
@@ -15,28 +12,24 @@ def dixon(N,B=7):
     return tmp
 
   base = primes(B)
-
-  #print(base)
-  
   start = isqrt(N)
   i = start
+
+  ii2N = []
   while i<=N:
-    for j in range(len(base)):
-      l = pow(i,2, N)
-      r = pow(base[j],2,N)
-      #print(i,j,l,r)
-      if l == r:
-        pairs.append([i,base[j]])
-        #print(pairs)
+    ii2N.append((i,pow(i,2,N)))
     i+=1
 
-  #print(pairs)
+  basej2N = []
+  for j in range(0,len(base)):
+    basej2N.append(pow(base[j],2,N))
 
-  for i in range(len(pairs)):
-    x = pairs[i][0]
-    y = pairs[i][1]
-    f = (gcd(x-y,N))
-    if 1 < f < N:
-      return f,N//f
+  for i,i2N in ii2N:
+    for k in range(0,len(base)):
+      if i2N == basej2N[k]:
+        f=gcd(i-base[k],N)
+        if 1 < f < N:
+          return f,N//f
+  return -1
 
 print(dixon(int(sys.argv[1])))
