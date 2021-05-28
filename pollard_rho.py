4431a8ef175f8e6ee0a145a3fc98c9a8a5c167a7
Darío Clavijo <clavijodario@gmail.com>
2021-05-28 20:44:28 -0300

    Update pollard_rho.py

--- pollard_rho.py
+++ pollard_rho.py
@@ -1,7 +1,7 @@
 from gmpy2 import isqrt, gcd, is_prime
 import sys
 
-def pollard_rho(self, n, seed=2, p=2, mode=1):
+def pollard_rho(self, n, seed=2, p=2, c=1):
         if is_prime(n):
             return n
         if n % 2 == 0:
@@ -10,15 +10,13 @@ def pollard_rho(self, n, seed=2, p=2, mode=1):
             return 3
         if n % 5 == 0:
             return 5
-        if mode == 1:
-            f = lambda x: x ** p + 1
-        else:
-            f = lambda x: x ** p - 1
+        f = lambda x: x ** p + c
         x, y, d = seed, seed, 1
         while d == 1:
             x = f(x) % n
             y = f(f(y)) % n
             d = gcd((x - y) % n, n)
-        return None if d == n else d
+            if N > d > 1:
+                return d
         
 print(int(sys.argv[1]))
