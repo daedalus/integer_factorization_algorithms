c1a29543d92c98c87e9534399ff9dd1137fdae77
dclavijo <clavijodario@gmail.com>
2023-03-10 19:04:55 -0300

    Improve Fermat attack algorithm by replacing isqrt with isqrt_rem and is_square.

--- fermat.py
+++ fermat.py
@@ -1,20 +1,16 @@
 import sys
-from gmpy2 import isqrt
+from gmpy2 import isqrt, isqrt_rem, is_square
 
 def fermat(n):
-        """Fermat attack"""
-        a = isqrt(n)
-        a = b
-        b2 = (a ** 2) - n
-        count = 0
-        while (b ** 2) != b2:
-            a = a + 1
-            b2 = (a ** 2) - n
-            b = isqrt(b2)
-            count += 1
-        p = a + b
-        q = a - b
-        assert n == p * q
-        return p, q
+    a, rem = isqrt_rem(n)
+    b2 = -rem
+    c0 = (a << 1) + 1
+    c = c0
+    while not is_square(b2):
+        b2 += c
+        c += 2
+    a = (c - 1) >> 1
+    b = isqrt(b2)
+    return a - b, a + b
 
 print(fermat(int(sys.argv[1])))
